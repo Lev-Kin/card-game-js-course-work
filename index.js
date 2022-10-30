7a2a9e1bdc59033812930d9e988a0e49c0c1ae35
Leva <parimalleva@gmail.com>
2022-10-30 11:32:27 +0300

    add fun startGame and work this fun, add fun flipCard and event handler

--- index.js
+++ index.js
@@ -15,27 +15,6 @@ const state = {
     loop: null
 }
 
-const generateGame = () => {
-    const dimensions = selectors.board.getAttribute('data-dimension');
-    const emojis = ['🍄', '🍇', '🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🥭', '🍎', '🍏', '🍐', '🍑', '🍒', '🍓', '🥝'];
-    const picks = pickRandom(emojis, (dimensions * 4) / 2);
-    const items = shuffle([...picks, ...picks]);
-    const cards = `
-        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
-            ${items.map(item => `
-                <div class="card">
-                    <div class="card-front"></div>
-                    <div class="card-back">${item}</div>
-                </div>
-            `).join('')}
-       </div>
-    `;
-
-    const parser = new DOMParser().parseFromString(cards, 'text/html');
-
-    selectors.board.replaceWith(parser.querySelector('.board'));
-}
-
 const pickRandom = (array, items) => {
     const clonedArray = [...array];
     const randomPicks = [];
@@ -63,4 +42,102 @@ const shuffle = array => {
     return clonedArray;
 }
 
+const generateGame = () => {
+    const dimensions = selectors.board.getAttribute('data-dimension');
+    const emojis = ['🍄', '🍇', '🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🥭', '🍎', '🍏', '🍐', '🍑', '🍒', '🍓', '🥝'];
+    const picks = pickRandom(emojis, (dimensions * 4) / 2);
+    const items = shuffle([...picks, ...picks]);
+    const cards = `
+        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `;
+
+    const parser = new DOMParser().parseFromString(cards, 'text/html');
+    selectors.board.replaceWith(parser.querySelector('.board'));
+}
+
+const startGame = () => {
+    state.gameStarted = true;
+    selectors.start.classList.add('disabled');
+
+    state.loop = setInterval(() => {
+        state.totalTime++;
+        selectors.moves.innerText = `${state.totalFlips} moves`;
+        selectors.timer.innerText = `time: ${state.totalTime} sec`;
+    }, 1000)
+}
+
+const flipBackCards = () => {
+    document.querySelectorAll('.card:not(.matched)').forEach(card => {
+        card.classList.remove('flipped');
+    })
+
+    state.flippedCards = 0;
+}
+
+const flipCard = card => {
+    state.flippedCards++;
+    state.totalFlips++;
+
+    if (!state.gameStarted) {
+        startGame();
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped');
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)');
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched');
+            flippedCards[1].classList.add('matched');
+            flippedCards[0].style.cssText = "visibility: hidden;";
+            flippedCards[1].style.cssText = "visibility: hidden;";
+        }
+
+        setTimeout(() => {
+            flipBackCards();
+        }, 1000);
+    }
+
+    if (!document.querySelectorAll('.card:not(.flipped)').length) {
+        setTimeout(() => {
+            selectors.boardContainer.classList.add('flipped');
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    You won!<br />
+                    with <span class="highlight">${state.totalFlips}</span> moves<br />
+                    under <span class="highlight">${state.totalTime}</span> seconds
+                </span>
+            `;
+
+            clearInterval(state.loop);
+        }, 1000);
+    }
+}
+
+const attachEventListeners = () => {
+    document.addEventListener('click', event => {
+        const eventTarget = event.target;
+        const eventParent = eventTarget.parentElement;
+
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent);
+        } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+            startGame();
+        }
+    })
+}
+
 generateGame();
+attachEventListeners();
+
+
