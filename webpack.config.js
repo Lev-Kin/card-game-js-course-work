7ee0b2eda115257453e81379bc4aaffcc4211e09
Leva <parimalleva@gmail.com>
2022-11-04 17:43:13 +0300

    fix webpack.config.js (add for React package.json)

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,72 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+let target = 'web';
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+    target = 'browserslist';
+}
+
+const plugins = [
+    new HtmlWebpackPlugin({
+
+    }),
+    new MiniCssExtractPlugin({
+
+    }),
+];
 
 module.exports = {
+    mode,
+    target,
+    plugins,
     entry: './index.js',
-    module: {
-        rules: [
-            { test: /\.svg$/, use: 'svg-inline-loader' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(js)$/, use: 'babel-loader' }
-        ]
-    },
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
+        clean: true,
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Bild',
-            inject: 'body'
-        })
-    ],
+    devServer: {
+        hot: true,
+    },
     stats: {
         children: true
     },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+                type: mode === 'production' ? 'asset' : 'asset/resource',
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    },
+                },
+            },
+        ],
+    },
 }
